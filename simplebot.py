05006acdc69efde4ba62a7a8cd2b8796738e36af
Abdul Haq <ahaq63334@gmail.com>
2023-12-26 06:43:20 +0000

    Finished The Simple Bots

--- simplebot.py
+++ simplebot.py
@@ -1,109 +1,55 @@
-import tkinter.scrolledtext as tks #creates a scrollable text window
-
-from datetime import datetime
-from tkinter import *
-import os
+import tkinter as tk
+from tkinter import scrolledtext
 import openai
-openai.api_key=os.getenv('B')
-
-# Generating response
-
-# Generating response
+from openai import OpenAI
+import Api #ther is a file name Api.py which have variable name Apikey and I have stored my apikey in this variable
 def get_bot_response(user_input):
-    prompt = f"Please provide a response to the following user input: '{user_input}'"
+    client = OpenAI(api_key=Api.Apikey)
 
-    response = openai.Completion.create(model="text-davinci-002",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.5,
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "user",
+        "content": user_input
+        }
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
     )
-
-    bot_response = response.choices[0].text.strip()
+    bot_response=response.choices[0].message.content
     return bot_response
 
+class ChatBotApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("ChatBot")
 
+        self.chat_history = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=15)
+        self.chat_history.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
 
-def create_and_insert_user_frame(user_input):
-  userFrame = Frame(chatWindow, bg="#d0ffff")
-  Label(
-      userFrame,
-      text=user_input,
-      font=("Arial", 11),
-      bg="#d0ffff").grid(row=0, column=0, sticky="w", padx=5, pady=5)
-  Label(
-      userFrame,
-      text=datetime.now().strftime("%H:%M"),
-      font=("Arial", 7),
-      bg="#d0ffff"
-  ).grid(row=1, column=0, sticky="w")
+        self.user_input = tk.Entry(root, width=30)
+        self.user_input.grid(row=1, column=0, padx=10, pady=10)
 
-  chatWindow.insert('end', '\n ', 'tag-right')
-  chatWindow.window_create('end', window=userFrame)
+        self.send_button = tk.Button(root, text="Send", command=self.send_message)
+        self.send_button.grid(row=1, column=1, padx=10, pady=10)
 
+        self.quit_button = tk.Button(root, text="Quit", command=root.destroy)
+        self.quit_button.grid(row=1, column=2, padx=10, pady=10)
 
-def create_and_insert_bot_frame(bot_response):
-  botFrame = Frame(chatWindow, bg="#ffffd0")
-  Label(
-      botFrame,
-      text=bot_response,
-      font=("Arial", 11),
-      bg="#ffffd0",
-      wraplength=400,
-      justify='left'
-  ).grid(row=0, column=0, sticky="w", padx=5, pady=5)
-  Label(
-      botFrame,
-      text=datetime.now().strftime("%H:%M"),
-      font=("Arial", 7),
-      bg="#ffffd0"
-  ).grid(row=1, column=0, sticky="w")
+    def send_message(self):
+        user_input = self.user_input.get().lower()
+        self.chat_history.insert(tk.END, "You: " + user_input + "\n")
 
-  chatWindow.insert('end', '\n ', 'tag-left')
-  chatWindow.window_create('end', window=botFrame)
-  chatWindow.insert(END, "\n\n" + "")
+        response = get_bot_response(user_input)
+        self.chat_history.insert(tk.END, "ChatBot: " + response + "\n")
 
+        self.user_input.delete(0, tk.END)  # Clear the user input field
 
-def send(event=None):
-    chatWindow.config(state=NORMAL)
-
-    user_input = userEntryBox.get("1.0",'end-2c')
-    user_input_lc = user_input.lower()
-    bot_response = get_bot_response(user_input_lc) 
-
-    create_and_insert_user_frame(user_input)
-    create_and_insert_bot_frame(bot_response)
-
-    chatWindow.config(state=DISABLED)
-    userEntryBox.delete("1.0","end")
-    chatWindow.see('end')
-
-
-baseWindow = Tk()
-baseWindow.title("The Simple Bot")
-baseWindow.geometry("500x250")
-
-chatWindow = tks.ScrolledText(baseWindow, font="Arial")
-chatWindow.tag_configure('tag-left', justify='left')
-chatWindow.tag_configure('tag-right', justify='right')
-chatWindow.config(state=DISABLED)
-
-sendButton = Button(
-    baseWindow,
-    font=("Verdana", 12, 'bold'),
-    text="Send",
-    bg="#fd94b4",
-    activebackground="#ff467e",
-    fg='#ffffff',
-    command=send)
-sendButton.bind("<Button-1>", send)
-baseWindow.bind('<Return>', send)
-
-userEntryBox = Text(baseWindow, bd=1, bg="white", width=38, font="Arial")
-
-chatWindow.place(x=1, y=1, height=200, width=500)
-userEntryBox.place(x=3, y=202, height=27)
-sendButton.place(x=430, y=200)
-
-baseWindow.mainloop()    
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ChatBotApp(root)
+    root.mainloop()
